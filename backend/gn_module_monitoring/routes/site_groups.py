8ed9b6efde1f898b1c6fe1c107486af8dca69c53
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2022-12-13 17:19:50 +0100

    feat(api): wip: add sites groups route

---
+++ backend/gn_module_monitoring/routes/site_groups.py
@@ -0,0 +1,20 @@
+
+from flask import request
+from werkzeug.datastructures import MultiDict
+
+from gn_module_monitoring.blueprint import blueprint
+from gn_module_monitoring.monitoring.models import TMonitoringSitesGroups
+from gn_module_monitoring.utils.routes import get_limit_offset, paginate
+
+
+@blueprint.route("/sites_groups", methods=["GET"])
+def get_site_groups():
+    params = MultiDict(request.args)
+    limit, page = get_limit_offset(params=params)
+
+    query = TMonitoringSitesGroups.query
+    if len(params) != 0:
+        query = query.filter_by(**params)
+    query = query.order_by(TMonitoringSitesGroups.id_sites_group)
+    return paginate(query=query, object_name="sites_groups", limit=limit, page=page)
+
