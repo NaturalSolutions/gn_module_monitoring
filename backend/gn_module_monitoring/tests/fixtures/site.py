69fef75d40adf53274b569f2933c24a2cd32c219
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2022-12-19 11:08:23 +0100

    tests: moved site_groups in tests and add tests

--- backend/gn_module_monitoring/tests/fixtures/site.py
+++ backend/gn_module_monitoring/tests/fixtures/site.py
@@ -5,17 +5,8 @@ from geonature.utils.env import db
 from pypnnomenclature.models import BibNomenclaturesTypes, TNomenclatures
 from shapely.geometry import Point
 
-from gn_module_monitoring.monitoring.models import (BibCategorieSite,
-                                                    TMonitoringSitesGroups)
+from gn_module_monitoring.monitoring.models import BibCategorieSite
 
-
-@pytest.fixture
-def sites_groups():
-    names = ["Site_eolien", "Site_Groupe"]
-
-    site_groups = {}
-    for name in names:
-        pass
 
 @pytest.fixture()
 def categories():
