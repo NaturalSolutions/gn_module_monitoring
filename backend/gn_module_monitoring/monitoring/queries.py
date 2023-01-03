213fa4202f8729384ac1354014a0e9c52ce34f9c
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2023-01-03 10:58:42 +0100

    feat(api): add sort and fix _get_model
    Via _get_entity

--- backend/gn_module_monitoring/monitoring/queries.py
+++ backend/gn_module_monitoring/monitoring/queries.py
@@ -4,13 +4,27 @@ from sqlalchemy import and_
 
 
 class Query(BaseQuery):
+    def _get_entity(self, entity):
+        if hasattr(entity, "_entities"):
+            return self._get_entity(entity._entities[0])
+        return entity.entities[0]
+
     def _get_model(self):
         # When sqlalchemy is updated:
         # return self._raw_columns[0].entity_namespace
-        return self._entities[0].selectable.c
+        # But for now:
+        entity = self._get_entity(self)
+        return entity.c
 
     def filter_by_params(self, params: MultiDict = None):
         model = self._get_model()
         and_query = and_(*tuple(getattr(model, key).ilike(f"%{value}%") for key, value in params.items()))
         return self.filter(and_query)
 
+    def sort(self, label: str, direction: str):
+        model = self._get_model()
+        order_by = getattr(model, label)
+        if direction == "desc":
+            order_by = order_by.desc()
+
+        return self.order_by(order_by)
