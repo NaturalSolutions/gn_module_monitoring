33bac9190073783db4c6ea2218ace261e211963a
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2023-04-13 14:25:24 +0200

    chore(api): put back old formatting

--- backend/gn_module_monitoring/monitoring/definitions.py
+++ backend/gn_module_monitoring/monitoring/definitions.py
@@ -6,35 +6,38 @@ from .models import (
     TMonitoringObservationDetails,
     TMonitoringSitesGroups,
 )
-from .objects import MonitoringModule, MonitoringSite
+from .objects import (
+    MonitoringModule,
+    MonitoringSite
+)
 
 from .base import monitoring_definitions, monitoring_g_definitions
 from .repositories import MonitoringObject
 from .geom import MonitoringObjectGeom
 
 
-"""
+'''
     MonitoringModels_dict :
     Fait le lien entre les monitoring_objects
     et les modèles sqlalchemy
-"""
+'''
 
 MonitoringModels_dict = {
-    "module": TMonitoringModules,
-    "site": TMonitoringSites,
-    "visit": TMonitoringVisits,
-    "observation": TMonitoringObservations,
-    "observation_detail": TMonitoringObservationDetails,
-    "sites_group": TMonitoringSitesGroups,
+    'module': TMonitoringModules,
+    'site': TMonitoringSites,
+    'visit': TMonitoringVisits,
+    'observation': TMonitoringObservations,
+    'observation_detail': TMonitoringObservationDetails,
+    'sites_group': TMonitoringSitesGroups,
 }
 
 MonitoringObjects_dict = {
-    "module": MonitoringModule,  # besoin pour retrouver le module depuis module_code à voir si on peux faire sans
-    "site": MonitoringSite,
-    "visit": MonitoringObject,
-    "observation": MonitoringObject,
-    "observation_detail": MonitoringObject,
-    "sites_group": MonitoringObjectGeom,
+    'module': MonitoringModule,  # besoin pour retrouver le module depuis module_code à voir si on peux faire sans
+    'site': MonitoringSite,
+    'visit': MonitoringObject,
+    'observation': MonitoringObject,
+    'observation_detail': MonitoringObject,
+    'sites_group': MonitoringObjectGeom,
 }
 
 monitoring_definitions.set(MonitoringObjects_dict, MonitoringModels_dict)
