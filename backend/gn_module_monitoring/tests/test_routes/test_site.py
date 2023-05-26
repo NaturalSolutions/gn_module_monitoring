055971a9b7bffb7281c913911702baef59fd905a
Andria Capai <andria_capai@natural-solutions.eu>
2023-05-26 11:24:51 +0200

    style: apply black

--- backend/gn_module_monitoring/tests/test_routes/test_site.py
+++ backend/gn_module_monitoring/tests/test_routes/test_site.py
@@ -149,7 +149,7 @@ class TestSite:
             "page": 1,
             "sort_label": "label_fr",
             "sort_dir": "asc",
-            "label_fr": string_contains
+            "label_fr": string_contains,
         }
         r = self.client.get(
             url_for("monitorings.get_types_site_by_label"), query_string=query_string
@@ -176,7 +176,7 @@ class TestSite:
             res.as_dict()["base_site_name"]
             == site_to_post_with_types["properties"]["base_site_name"]
         )
-    
+
     def test_delete_site(self, sites):
         site = list(sites.values())[0]
         id_base_site = site.id_base_site
