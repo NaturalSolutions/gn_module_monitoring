098a7b8829b46ca8c2de0de25546feadf374b560
Natural Solutions <geonature_support@natural-solutions.eu>
2024-01-30 11:32:33 +0100

    fix: reorder migrations

--- backend/gn_module_monitoring/migrations/c1528c94d350_upgrade_existing_permissions.py
+++ backend/gn_module_monitoring/migrations/c1528c94d350_upgrade_existing_permissions.py
@@ -10,13 +10,23 @@ import sqlalchemy as sa
 
 from click.testing import CliRunner
 
+from gn_module_monitoring.command.cmd import process_available_permissions
+from gn_module_monitoring.command.utils import installed_modules
+
 # revision identifiers, used by Alembic.
 revision = "c1528c94d350"
-down_revision = "3ffeea74a9dd"
+down_revision = "398f94b364f7"
 branch_labels = None
 depends_on = None
 
 def upgrade():
+    bind = op.get_bind()
+    session = sa.orm.Session(bind=bind)
+
+    # Création des permissions disponibles pour chaque module
+    for module in installed_modules(session):
+        process_available_permissions(module["module_code"], session=session)
+
     # ########
     # Mise à jour des permissions existantes vers les sous objets
     # Création des permission des sous-objets à partir des permissions contenus dans l'objet ALL
